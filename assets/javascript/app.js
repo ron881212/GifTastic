1ac634acbdd88b226ff2a294fb09ab5f016d7790
ron881212 <ron881212@gmail.com>
2018-12-11 10:56:18 -0500

    added gif still and animate to card

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,7 +9,8 @@ var limit =     "limit=5";
 var userInput;
 var subject;
 var queryURL;
-
+var gifCards;
+var gifBox;
 // FUNCTIONS
 //============================================================
 function getGifs(subject){
@@ -19,12 +20,22 @@ function getGifs(subject){
     })
       .then(function(response){
         // console.log(response.data[2].images.fixed_height_still.url);
-        // console.log(urlBase + subject + apikey);
+        console.log(urlBase + subject + apikey);
             for(let i = 0; i < 5; i++){
-                var gifResponse = response.data[i].images.fixed_height_still.url;
-                var gif = $("<img>");
-                gif.attr("src", gifResponse);
-                $("#gifsHere").append(gif);
+                var gifStill = response.data[i].images.fixed_height_still.url;
+                var gifAnimate = response.data[i].images.fixed_height.url;
+                var gifRating = response.data[i].rating;
+                // gif.attr("src", gifResponse);
+                // $("#gifsHere").append(gif);
+                gifBox = $("<div>");
+                gifCards = ('<div class="card mt-3" style="width: 18rem;">' +
+                '<img class="card-img-top" src="' + gifStill +'" data-still="" data-animate="' + gifAnimate +'" data-type="" alt="Card image cap">' +
+                '<div class="card-body">' + '<h5 class="card-title">Rating: '+ gifRating +'</h5>' +
+                '<button type="submit" class="btn btn-primary playGif mr-3">Play Gif</button>' +
+                '<button id="saveToFav" type="submit" class="btn btn-success">Save To Favorite</button>' +
+                '</div>' + '</div>');
+                $(gifBox).append(gifCards);
+                $("#gifsHere").append(gifBox);
             }
       });
 }
@@ -32,27 +43,34 @@ function getGifs(subject){
 // MAIN PROCESSES
 //============================================================
 
-$(document).on("click", "#searchGifs", function(){
+// Function to make buttons out of inputs
+$(document).on("click", "#searchGifs", function(event){
+    event.preventDefault();
+    if($("#userInput").val() !== ""){
 
-    $(".lead").empty();
-    topic.push($("#userInput").val());
+        $(".lead").empty();
+        topic.push($("#userInput").val());
 
-    for(let i =0; i < topic.length; i++){
-    var newButton = $("<button>");
-    newButton.attr("type", "button");
-    newButton.attr("data-type", topic[i]);
-    newButton.addClass("topic btn btn-info mr-1 mt-1");
-    newButton.text(topic[i]);
-    $(".lead").append(newButton);
+        for(let i =0; i < topic.length; i++){
+            var newButton = $("<button>");
+            newButton.attr("type", "button");
+            newButton.attr("data-type", topic[i]);
+            newButton.addClass("topic btn btn-info mr-1 mt-1");
+            newButton.text(topic[i]);
+            $(".lead").append(newButton);
+        }
     }
+    // console.log($("#userInput").val());
     
 });
 
-$(document).on("click", "#clearGifs", function(){
+$(document).on("click", "#clearGifs", function(event){
+    event.preventDefault();
     $(".lead").empty();
 });
 
-$(document).on("click", ".topic", function(){
+$(document).on("click", ".topic", function(event){
+    event.preventDefault();
     var subject = $(this).attr("data-type");
     console.log($(this).attr("data-type"));
     getGifs(subject);
